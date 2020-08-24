c649e0795b1c2be90fb9403b87c5c4a53b36f9df
Pramod Gobburi <pramod.gobburi@gohpr.app>
2020-08-24 09:57:37 -0500

    Updates

---
+++ precache-manifest.a20292b7ef55c88511ffbafb6aecacfc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492fec25b0e24be14b11cd85c3391fdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b72ba4eb9774d2e4ab0",
+    "url": "/static/css/2.012b95f9.chunk.css"
+  },
+  {
+    "revision": "f391f670fae3b07168f0",
+    "url": "/static/css/main.d23e166e.chunk.css"
+  },
+  {
+    "revision": "0b72ba4eb9774d2e4ab0",
+    "url": "/static/js/2.e07f6916.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/static/js/2.e07f6916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f391f670fae3b07168f0",
+    "url": "/static/js/main.c6e660d3.chunk.js"
+  },
+  {
+    "revision": "4f84595f49be4b6cb752",
+    "url": "/static/js/runtime-main.fd4c6d0c.js"
+  },
+  {
+    "revision": "041e0fb2376f7b5952fc29d4a9b19fe1",
+    "url": "/static/media/embark.041e0fb2.png"
+  },
+  {
+    "revision": "ac13c2a42aebbaa2f50a1e5400d308e3",
+    "url": "/static/media/embark_logo.ac13c2a4.png"
+  }
+]);
